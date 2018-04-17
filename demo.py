8039da3dc59f14d8108be01f0413fe991010cb34
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2018-04-17 14:24:29 -0400

    Updated

--- demo.py
+++ demo.py
@@ -1,15 +1,52 @@
 # Ben's Mods Demo Script
 # (C)2018 - Benjamin Sykes
 
-print("Importing modules . . . ")
+print("Ben's Mods Demo")
+print("(C)2018 - Ben Sykes")
+print("")
+
+print("Setting variables . . . ", end='')
+quitall = False
+menu = "main"
+print("Done.")
+print("")
+
+print("Importing custom modules . . . ")
 print("- fibo")
 import fibo
-
+print("- smath")
+import smath
 print("")
 
-print("Testing \"fibo\" module . . . ")
-fibarray = fibo.genfib(100000)
-print(fibarray)
+while not(quitall):
 
-print("")
+    while menu == "main":
+        print("MAIN MENU")
+        print("[0] Quit")
+        print("[1] \"fibo\" Demo")
+        print("[2] \"smath\" Terminal")
+        option = input("(0-2)> ")
+        print("")
+        if option == "0":
+            quitall = True
+            menu = "quit"
+        elif option == "1":
+            menu = "fibodemo"
+        elif option == "2":
+            menu = "smathterm"
+        else:
+            print("Invalid option.")
+            print("")
 
+    while menu == "fibodemo":
+        print("\"FIBO\" DEMO")
+        print(str(fibo.genfib(1000)))
+        print("")
+        menu = "main"
+
+    while menu == "smathterm":
+        print("\"SMATH\" TERMINAL")
+        print("")
+        smath.demoterm()
+        print("")
+        menu = "main"
--- smath.py
+++ smath.py
@@ -1,5 +1,174 @@
 # Ben's Mods "smath" Module
 # (C)2018 - Ben Sykes
 
-def solvepythag(a, b, c, var2solve):
-    if (var2solve == "a") or (var2solve == "A"):
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def power(num, exp):
+    return pow(num, exp)
+
+def squareroot(num):
+    return sqrt(num)
+
+def demoterm():
+    smath_quitdemo = False
+    smath_demofirst = True
+    while not(smath_quitdemo):
+
+        if not(smath_demofirst):
+            print("")
+
+        smath_demofirst = False
+        smath_democommand = input("CMD~> ").lower()
+
+        if smath_democommand == "":
+            smath_demofirst = True
+
+        elif smath_democommand == "quit":
+            smath_quitdemo = True
+
+        elif smath_democommand == "add":
+            smath_demonum1ok = True
+            try:
+                smath_demonum1 = float(input("NUM1? "))
+            except TypeError:
+                smath_demonum1ok = False
+                print("Invalid type.")
+            except ValueError:
+                smath_demonum1ok = False
+                print("Invalid value.")
+            if smath_demonum1ok:
+                smath_demonum2ok = True
+                try:
+                    smath_demonum2 = float(input("NUM2? "))
+                except TypeError:
+                    smath_demonum2ok = False
+                    print("Invalid type.")
+                except ValueError:
+                    smath_demonum2ok = False
+                    print("Invalid value.")
+            if smath_demonum1ok and smath_demonum2ok:
+                print("= ", end='')
+                print(str(add(smath_demonum1,smath_demonum2)))
+
+        elif smath_democommand == "subtract":
+            smath_demonum1ok = True
+            try:
+                smath_demonum1 = float(input("NUM1? "))
+            except TypeError:
+                smath_demonum1ok = False
+                print("Invalid type.")
+            except ValueError:
+                smath_demonum1ok = False
+                print("Invalid value.")
+            if smath_demonum1ok:
+                smath_demonum2ok = True
+                try:
+                    smath_demonum2 = float(input("NUM2? "))
+                except TypeError:
+                    smath_demonum2ok = False
+                    print("Invalid type.")
+                except ValueError:
+                    smath_demonum2ok = False
+                    print("Invalid value.")
+            if smath_demonum1ok and smath_demonum2ok:
+                print("= ", end='')
+                print(str(subtract(smath_demonum1,smath_demonum2)))
+
+        elif smath_democommand == "multiply":
+            smath_demonum1ok = True
+            try:
+                smath_demonum1 = float(input("NUM1? "))
+            except TypeError:
+                smath_demonum1ok = False
+                print("Invalid type.")
+            except ValueError:
+                smath_demonum1ok = False
+                print("Invalid value.")
+            if smath_demonum1ok:
+                smath_demonum2ok = True
+                try:
+                    smath_demonum2 = float(input("NUM2? "))
+                except TypeError:
+                    smath_demonum2ok = False
+                    print("Invalid type.")
+                except ValueError:
+                    smath_demonum2ok = False
+                    print("Invalid value.")
+            if smath_demonum1ok and smath_demonum2ok:
+                print("= ", end='')
+                print(str(multiply(smath_demonum1,smath_demonum2)))
+
+        elif smath_democommand == "divide":
+            smath_demonum1ok = True
+            try:
+                smath_demonum1 = float(input("NUM1? "))
+            except TypeError:
+                smath_demonum1ok = False
+                print("Invalid type.")
+            except ValueError:
+                smath_demonum1ok = False
+                print("Invalid value.")
+            if smath_demonum1ok:
+                smath_demonum2ok = True
+                try:
+                    smath_demonum2 = float(input("NUM2? "))
+                except TypeError:
+                    smath_demonum2ok = False
+                    print("Invalid type.")
+                except ValueError:
+                    smath_demonum2ok = False
+                    print("Invalid value.")
+            if smath_demonum1ok and smath_demonum2ok:
+                print("= ", end='')
+                print(str(divide(smath_demonum1,smath_demonum2)))
+
+        elif smath_democommand == "power":
+            smath_demonum1ok = True
+            try:
+                smath_demonum1 = int(input("NUM~? "))
+            except TypeError:
+                smath_demonum1ok = False
+                print("Invalid type.")
+            except ValueError:
+                smath_demonum1ok = False
+                print("Invalid value.")
+            if smath_demonum1ok:
+                smath_demonum2ok = True
+                try:
+                    smath_demonum2 = int(input("EXP? "))
+                except TypeError:
+                    smath_demonum2ok = False
+                    print("Invalid type.")
+                except ValueError:
+                    smath_demonum2ok = False
+                    print("Invalid value.")
+            if smath_demonum1ok and smath_demonum2ok:
+                print("= ", end='')
+                print(str(power(smath_demonum1,smath_demonum2)))
+
+        elif smath_democommand == "sqrt":
+            smath_demonum1ok = True
+            try:
+                smath_demonum1 = float(input("NUM~? "))
+            except TypeError:
+                smath_demonum1ok = False
+                print("Invalid type.")
+            except ValueError:
+                smath_demonum1ok = False
+                print("Invalid value.")
+            if smath_demonum1ok:
+                print("= ", end='')
+                print(str(squareroot(smath_demonum1)))
+
+        else:
+            print("Invalid command.")
